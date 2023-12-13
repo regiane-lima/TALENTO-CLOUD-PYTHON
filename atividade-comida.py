a3559204b77f5640a7d5ea27ad1c03b75bd6ed02
Regiane Lima <120677112+regiane-lima@users.noreply.github.com>
2023-12-13 20:57:45 -0300

    Create atividade-comida.py

---
+++ atividade-comida.py
@@ -0,0 +1,8 @@
+almoco_favorito = 'Strogonoff com batata palha'
+almoco_preco = 25
+sobremesa_favorita = 'beijihos'
+preco_sobremesa = 10
+orcamento = 130
+total = almoco_preco + preco_sobremesa
+print(total)
+print(orcamento - total)
