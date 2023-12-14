7a6c95f8a03e9f551e10a9731f85f76bb22bac55
Regiane Lima <120677112+regiane-lima@users.noreply.github.com>
2023-12-13 21:04:48 -0300

    Update atividade-comida.py

--- atividade-comida.py
+++ atividade-comida.py
@@ -1,6 +1,6 @@
 almoco_favorito = 'Strogonoff com batata palha'
 almoco_preco = 25
-sobremesa_favorita = 'beijihos'
+sobremesa_favorita = 'beijinhos'
 preco_sobremesa = 10
 orcamento = 130
 total = almoco_preco + preco_sobremesa
