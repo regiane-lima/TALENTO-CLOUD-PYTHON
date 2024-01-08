24ebbe6c6d7b91281295204cc78fbbd6177cfd12
Regiane Lima <120677112+regiane-lima@users.noreply.github.com>
2024-01-08 18:26:11 -0300

    Create hotel.py

---
+++ hotel.py
@@ -0,0 +1,13 @@
+# Utilizando o loop for
+print("Utilizando o loop for:")
+for andar in range(20, 0, -1):
+    if andar != 13:
+        print(andar)
+
+# Utilizando o loop while
+print("\nUtilizando o loop while:")
+andar = 20
+while andar >= 1:
+    if andar != 13:
+        print(andar)
+    andar -= 1
